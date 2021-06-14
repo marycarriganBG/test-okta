81f09e6f3391fc73ea467fdbfafe0b25d6fce3d3
marycarrigan <mary.carrigan@billgo.com>
2021-06-14 16:04:29 -0600

    add

---
+++ src/config.js
@@ -0,0 +1,24 @@
+const oktaAuthConfig = {
+    // Note: If your app is configured to use the Implicit flow
+    // instead of the Authorization Code with Proof of Code Key Exchange (PKCE)
+    // you will need to add `pkce: false`
+    issuer: 'https://billgo-carrigan.okta.com/oauth2/default',
+    clientId: '0oa10e0i9kBYcJfbj5d7',
+    redirectUri: window.location.origin + '/login/callback',
+};
+
+const oktaSignInConfig = {
+    baseUrl: 'https://billgo-carrigan.okta.com',
+    clientId: '0oa10e0i9kBYcJfbj5d7',
+    redirectUri: window.location.origin + '/login/callback',
+    logo: '//logo.clearbit.com/billgo.com',
+    authParams: {
+        // If your app is configured to use the Implicit flow
+        // instead of the Authorization Code with Proof of Code Key Exchange (PKCE)
+        // you will need to uncomment the below line
+        // pkce: false
+    }
+    // Additional documentation on config options can be found at https://github.com/okta/okta-signin-widget#basic-config-options
+};
+
+export { oktaAuthConfig, oktaSignInConfig };
